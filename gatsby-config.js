dadf35363d0587b5dfc5dfc39845bdaee4db77c4
Yash Brahmkshatriya <yash311000@gmail.com>
2021-09-11 17:07:30 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio",
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"],
+};
