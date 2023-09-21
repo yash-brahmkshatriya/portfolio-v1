2cbf2eb93c1020e94a71ff12ccd2f195456c8d5c
Yash Brahmkshatriya <yash311000@gmail.com>
2023-09-21 18:28:25 +0530

    Updated redirect config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,10 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = ({ actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
-    fromPath: '/*',
+    fromPath: '/**',
     toPath: 'https://yashb.vercel.app',
-    statusCode: 301,
+    statusCode: 200,
+    isPermanent: true,
   });
 };
