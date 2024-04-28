12efa70cc69455107d33c5005669864fd9e79d32
Yash Brahmkshatriya <brahyash@Yashs-MacBook-Air.local>
2024-04-28 12:32:10 +0530

    updated content

--- src/components/About/AboutParagraph.tsx
+++ src/components/About/AboutParagraph.tsx
@@ -4,14 +4,15 @@ const AboutParagraph = () => {
   return (
     <div className="text-sm text-justify font-body sm:text-base">
       <p className="mb-4">
-        Hello there! I'm Yash, I'm a developer based in Pune, India. My current
-        work as a <span className="font-bold">Senior Analyst</span> at Deutsche
-        Bank involves the migration of fullstack applications.
+        Hello there! I'm Yash, I'm a developer, enjoying life in Pune, and
+        sometime in my native, Gandhinagar. I'm currently working as Software
+        engineer at Deutsche Bank.
       </p>
       <p className="mb-1 text-secondary">Some fun facts about me ?</p>
       <p className="mb-4">
-        Thriller, mystery, and sci-fi movies and series are my kinda things.
-        Also, I love solving chess puzzles and playing rapid chess.
+        Thriller, mystery, and sci-fi content are my kinda things. Also, I love
+        solving chess puzzles and playing rapid chess. I play pool and snooker
+        as well.
       </p>
     </div>
   );
